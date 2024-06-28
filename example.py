2a38e9739f4bf36ea7d6f16f0ac58c05b149d61f
Elliot <3186037+elliot-100@users.noreply.github.com>
2024-06-28 16:59:30 +0100

    add example 32 x 32 heightmap

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 """Example script."""
 
-from heightmap_renderer import EXAMPLE_HEIGHTMAP_16
+from heightmap_renderer import EXAMPLE_HEIGHTMAP_32 as EXAMPLE_HEIGHTMAP
 from heightmap_renderer.simple_renderer import SimpleRenderer
 
-my_render = SimpleRenderer(heightmap=EXAMPLE_HEIGHTMAP_16, scale=32)
+my_render = SimpleRenderer(heightmap=EXAMPLE_HEIGHTMAP, scale=32)
 my_render.show()
