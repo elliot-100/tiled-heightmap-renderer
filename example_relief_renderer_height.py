f8dff938170ef049bbb4f3aa76141cbd2eda0c23
Elliot <3186037+elliot-100@users.noreply.github.com>
2024-07-07 13:37:07 +0100

    Add: split demos for height and depth renderers

---
+++ example_relief_renderer_height.py
@@ -0,0 +1,13 @@
+"""Example script."""
+
+from heightmap_renderer import EXAMPLE_HEIGHTMAP_32 as EXAMPLE_HEIGHTMAP
+from heightmap_renderer.tiled_relief_renderer import TiledReliefRenderer
+
+my_render = TiledReliefRenderer(
+    heightmap=EXAMPLE_HEIGHTMAP,
+    scale=32,
+    relief_scale=0.5,
+    shader="height",
+    debug_renderer=False,
+)
+my_render.show()
