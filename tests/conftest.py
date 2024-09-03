1b7ebf7d52d75f49cb65d45ec91fa3d284db80bd
Elliot <3186037+elliot-100@users.noreply.github.com>
2024-09-03 17:17:50 +0100

    Lint with ruff 0.6.3

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ import pytest
 from heightmap_renderer import EXAMPLE_HEIGHTMAP_16
 
 
-@pytest.fixture()
+@pytest.fixture
 def sample_heightmap_16() -> list[list[int]]:
     """Return a sample heightmap."""
     return EXAMPLE_HEIGHTMAP_16
